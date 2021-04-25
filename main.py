c282f0935124a6f43a30a0b1ac1d2ada96ab0798
Aadit Trived <aadit.trivedi03@gmail.com>
2021-04-24 20:34:03 -0700

    a

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/add', methods=["POST", "GET"])
+def add():
+    return render_template("add.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
