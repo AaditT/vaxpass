720ab8dd2170d0efc17e7d08fc4430fc0d831501
Aadit Trived <aadit.trivedi03@gmail.com>
2021-04-24 21:02:21 -0700

    finished_input_form

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from flask import *
+from werkzeug.utils import secure_filename
+import os
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = 'cards/'
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'key'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 
 @app.route('/')
 def home():
@@ -8,7 +17,18 @@ def home():
 
 @app.route('/add', methods=["POST", "GET"])
 def add():
+    if request.method == "POST":
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('add',
+                                    filename=filename))
     return render_template("add.html")
 
 if __name__ == '__main__':
+    
     app.run(debug=True)
